f7c04cb596b43e6e2bd7d3b74222744bbb1765d6
Bernd Lehmann <bernd@lehmann.be>
2017-11-23 23:28:05 +0100

    added roles

--- staging.sh
+++ staging.sh
@@ -2,8 +2,6 @@
 
 EXIT=0
 vagrant up --color <<< 'boot' || EXIT=$?
-sleep 60
-vagrant up --color
 export ANSIBLE_FORCE_COLOR=true
 ansible-playbook ../site.yml <<< 'ansible playbook' || EXIT=$?
 vagrant destroy -f
