d8ff7b0e1c21141e79e5c95cff6a02393ff4210d
Bernd Lehmann <bernd@lehmann.be>
2017-11-24 12:07:09 +0100

    add connectivity check

--- staging.sh
+++ staging.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 EXIT=0
-vagrant up --color <<< 'boot' || EXIT=$?
+vagrant up --color <<< 'boot' || EXIT=$?i
+sleep 30
 export ANSIBLE_FORCE_COLOR=true
 ansible-playbook ../site.yml <<< 'ansible playbook' || EXIT=$?
+sleep 30
+ansible-playbook ../cisco_check_icmp.yml <<< 'connectivity check' || EXIT=$?
 vagrant destroy -f
 echo $EXIT
 exit $EXIT
