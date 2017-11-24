8330208cb86ba747e43a5dd92057e96c07523257
Bernd Malmqvist <bernd@lehmann.be>
2017-11-24 19:30:41 +0000

    changed staging.sh

--- staging.sh
+++ staging.sh
@@ -5,7 +5,7 @@ vagrant up --color <<< 'boot' || EXIT=$?i
 sleep 30
 export ANSIBLE_FORCE_COLOR=true
 ansible-playbook ../site.yml <<< 'ansible playbook' || EXIT=$?
-sleep 30
+sleep 60
 ansible-playbook ../cisco_check_icmp.yml <<< 'connectivity check' || EXIT=$?
 vagrant destroy -f
 echo $EXIT
