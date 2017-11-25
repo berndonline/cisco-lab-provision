67813ab5eea9a9335ef25ad968b6bee2fa7d5570
Bernd Lehmann <bernd@lehmann.be>
2017-11-25 13:39:00 +0100

    changed yml

--- staging.sh
+++ staging.sh
@@ -5,7 +5,7 @@ vagrant up --color <<< 'boot' || EXIT=$?i
 sleep 30
 export ANSIBLE_FORCE_COLOR=true
 ansible-playbook ../site.yml <<< 'ansible playbook' || EXIT=$?
-sleep 60
+sleep 120
 ansible-playbook ../cisco_check_icmp.yml <<< 'connectivity check' || EXIT=$?
 vagrant destroy -f
 echo $EXIT
